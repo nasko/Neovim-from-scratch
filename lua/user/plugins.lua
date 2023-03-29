2d16b88ddb8ce1eb919d5743534c536b3eff9950
Atanas Vasilev <atanas.vasilev@gmail.com>
2023-03-29 22:17:52 +0300

    Install vim-fugitive plugin - a wrapper around Git for Vim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -68,6 +68,8 @@ return packer.startup(function(use)
   use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
   use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
 
+  -- Fugitive is the premier Vim plugin for Git
+  use 'tpope/vim-fugitive'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
